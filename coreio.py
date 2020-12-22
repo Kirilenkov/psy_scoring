06bc414d348420be8331ecc01f9727f8045b7693
kirill <kkirilenkov@gmail.com>
2020-12-22 23:47:53 +0700

    + participant data

--- coreio.py
+++ coreio.py
@@ -2,17 +2,23 @@ import pandas as pd
 # import main_objects
 import openpyxl as ex
 from inputpath import *
+from participant_input import main
 
 PATH = '/Users/kirill/pr/FFM/psyscoring'
 path_setter(PATH)
 
-sheets_amount = len(ex.load_workbook('Scales.xlsx').worksheets)
+wb = ex.load_workbook('Scales.xlsx')
+scales_names = wb.sheetnames
+sheets_amount = len(scales_names)
 scales_df = []
 
 for i in range(sheets_amount):
     scales_df.append(pd.read_excel('Scales.xlsx', engine='openpyxl', sheet_name=i))
 
-print(scales_df)
+scales_names = wb.sheetnames
+
+main()
+
 
 
 
---
+++ participant_input.py
@@ -0,0 +1,121 @@
+from datetime import datetime as dt
+
+
+class NoMatch(Exception):
+    pass
+
+
+class LenNoMatch(NoMatch):
+    pass
+
+
+KEY = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+EOI = 'Ошибка ввода. '
+
+
+def letter_encoder(letter, decoding):
+    result = str(decoding.find(letter) + 1)
+    if len(result) == 1:
+        result = '0' + result
+    return result
+
+
+def exc_check(sequence, key):
+    e = 0
+    for letter in sequence:
+        if not letter.upper() in key:
+            print(EOI + 'Недопустимый символ: "{0:s}"'.format(letter))
+            e += 1
+    if e != 0:
+        raise NoMatch
+
+
+def input_checker(content):
+    while True:
+        output = input('{} \n'.format(content)).upper()
+        try:
+            exc_check(output, KEY)
+        except NoMatch:
+            continue
+        break
+    return output
+
+
+def full_name_input():
+    parts = ['фамилию ', 'имя ', 'отчество ']
+    full_name = ''
+    for p in parts:
+        full_name += input_checker('Введите ' + p + 'пациента:') + ' '
+    return full_name[:-1]
+
+
+def check_date(*messages, check_range=False):
+    mess = messages
+    date = ''
+    while True:
+        try:
+            date = input(mess[0])
+            if len(date) != 10:
+                raise LenNoMatch
+        except ValueError:
+            print(EOI + 'Недопустимые символы')
+            continue
+        except LenNoMatch:
+            print(EOI + 'Длина ввода должна был 10 символов вместе с разделителями')
+            continue
+        else:  # Изёвая проверка корректности даты. Можно строже.
+            if int(date[0:2]) < 1 or int(date[0:2]) > 31:
+                print(EOI + 'Число месяца должо быть в диапазоне [1, 31].')
+                continue
+            if int(date[3:5]) < 1 or int(date[3:5]) > 12:
+                print(EOI + 'Месяц должен быть в диапазоне [1, 12].')
+                continue
+                # Обсудить рамки возраста тестируемых
+            year = dt.today().year
+            if check_range and (int(date[6:]) < year - 110 or int(date[6:]) > year - 10):
+                print(EOI + mess[1].format(year - 110, year - 10))
+                continue
+        break
+
+    return date
+
+
+def report():
+    full_name = full_name_input()
+    dob_mess = ('Введите день и месяц и год рождения испытуемого в формате дд.мм.гггг: \n',
+                'Год рождения должен быть в диапазоне [{0:d}, {1:d}].')
+    dob = check_date(*dob_mess, check_range=True)
+    fd_mess = ('Введите дату заполнения опросников в формате дд.мм.гггг: \n',)
+    filling_date = check_date(*fd_mess)
+
+    print('Вы ввели ФИО: {!r}'.format(full_name.upper()))
+    print('Вы ввели дату рождения: {!r}'.format(dob))
+    print('Вы ввели дату заполнения опросников {!r}'.format(filling_date))
+    print('Нажмите Enter для продолжения.')
+    input()
+    return {'full_name': full_name, 'dob': dob, 'filling_date': filling_date}
+
+
+def visit_input():
+    visit = 0
+    while True:
+        try:
+            visit = int(input('Введите номер визита (от 1 до 3): \n'))
+            if visit < 1 or visit > 3:
+                print(EOI + 'Номер визита должен быть в диапазоне [1, 3]')
+                continue
+        except ValueError:
+            print(EOI + 'Введите ЧИСЛО от 1 до 3.')
+            continue
+        break
+    return visit
+
+
+def main():
+    primary = report()
+    primary['visit'] = str(visit_input())
+    return primary
+
+
+if __name__ == '__main__':
+    main()
